1742f4fdc87f3a848cfcb56a91f5b55d89e60202
bedirlituran <120916975+bedirlituran@users.noreply.github.com>
2024-02-05 04:01:01 +0400

    IP

--- components/Slider.jsx
+++ components/Slider.jsx
@@ -1,7 +1,10 @@
 // components/Carousel.js
 "use client"; 
 // components/Carousel.js
-import { useState, useEffect } from 'react';
+import React from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 import Image from 'next/image';
 
 const images = [
@@ -12,27 +15,27 @@ const images = [
   '/image/sekil5.png',
 ];
 
+const settings = {
+  dots: true,
+  infinite: true,
+  speed: 3000, // 2 saniyede bir geçiş
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000, // 2 saniyede bir otomatik geçiş
+};
+
 const Carousel = () => {
-  const [currentImage, setCurrentImage] = useState(0);
-
-  useEffect(() => {
-    const timeoutId = setTimeout(() => {
-      setCurrentImage((prevImage) => (prevImage + 1) % images.length);
-    }, 3000);
-
-    return () => clearTimeout(timeoutId);
-  }, [currentImage]);
-
   return (
     <>
-      <h1 className='text-center text-5xl font-bold mb-8'>Kompaniyalar</h1>
-      <div className="carousel carousel-center p-4 space-x-4 bg-gradient-to-r from-blue-800 via-gray-900 to-gray-800 rounded-box flex justify-center items-center w-[94%] m-auto mb-10">
+      <h1 className='text-center text-4xl font-bold mb-8'>Kompaniyalar</h1>
+      <Slider {...settings} className='flex items-center justify-center bg-gradient-to-r from-gray-800 via-gray-900 to-blue-800 border border-black p-4 mb-10'>
         {images.map((image, index) => (
-          <div key={index} className={`carousel-item ${index === currentImage ? 'active' : ''}`}>
-            <Image src={image} width={400} height={800} alt={`Slide ${index + 1}`} className="rounded-box" />
+          <div key={index} className="carousel-item ">
+            <Image src={image} width={400} height={800} alt={`Slide ${index + 1}`} className="ml-2 rounded-box" />
           </div>
         ))}
-      </div>
+      </Slider>
     </>
   );
 };
