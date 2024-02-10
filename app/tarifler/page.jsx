39dfca36431a938f38e664185acbf0c563f87bde
bedirlituran <120916975+bedirlituran@users.noreply.github.com>
2024-02-10 16:47:10 +0400

    IP

--- app/tarifler/page.jsx
+++ app/tarifler/page.jsx
@@ -6,11 +6,8 @@ const Page = () => {
   const [price, setPrice] = useState(0);
   const qiymetRef = useRef();
 
-  // Seçilen paket değiştikçe fiyatı güncelle
   useEffect(() => {
     if (selectedPackage !== null) {
-      // Burada seçilen paketin fiyatını hesaplayabilir veya bir API'den alabilirsiniz
-      // Bu örnekte, seçilen paketlerin sabit fiyatları olduğunu varsayıyoruz
       switch (selectedPackage) {
         case "3azn":
           setPrice(3);
@@ -37,12 +34,9 @@ const Page = () => {
     }
   }, [selectedPackage]);
 
-  // Checkbox değiştiğinde seçilen paketi güncelle
   const handleCheckboxChange = (e) => {
     const clickedPackage = e.target.value;
 
-    // Eğer seçilen paket mevcutsa ve mevcut seçilen paketle aynı değilse,
-    // seçilen paketi güncelle.
     if (selectedPackage !== null && selectedPackage !== clickedPackage) {
       setSelectedPackage(clickedPackage);
     } else if (selectedPackage === null) {
@@ -51,10 +45,10 @@ const Page = () => {
   };
 
   return (
-    <div className="p-20">
-      <div className="flex-col items-center justify-start  p-24 text-black shadow-2xl border border-gray-400 rounded-xl">
+    <div className="container mx-auto p-5 md:p-20">
+      <div className="flex flex-col items-center justify-start p-5 md:p-24 text-black shadow-2xl border border-gray-400 rounded-xl">
         <div className="flex justify-center items-center">
-          <h1 className="font-bold text-center md:text-3xl mb-10">
+          <h1 className="font-bold text-center md:text-3xl mb-5 md:mb-10">
             Şəxsi məlumatlarını qeyd et
           </h1>
         </div>
@@ -62,30 +56,46 @@ const Page = () => {
           <input
             type="text"
             placeholder="Ad"
-            className="input input-bordered placeholder:text-black w-[150] bg-gray-100"
+            className="input input-bordered placeholder:opacity-50 w-full md:w-[300px] bg-gray-100 mb-2"
           />
-          <label htmlFor="soyad"></label>
           <input
             type="text"
             placeholder="Soyad"
-            className="input input-bordered placeholder:text-black w-[150] bg-gray-100"
+            className="input input-bordered placeholder:opacity-50 w-full md:w-[300px] bg-gray-100 mb-2"
           />
-          <label htmlFor="mobil"></label>
-          <input
-            type="mobile"
-            placeholder="Mobil"
-            className="input input-bordered placeholder:text-black w-[150] bg-gray-100"
-          />
-          <button className="p-3 bg-yellow-400 text-black rounded-xl ">
+          <div className="flex items-center mb-2">
+            <select
+              className="input input-bordered placeholder:opacity-50 w-[100px] bg-gray-100 mr-2"
+              defaultValue=""
+            >
+              <option value="" disabled hidden>
+                Kod
+              </option>
+              <option value="099">099</option>
+              <option value="055">055</option>
+              <option value="051">051</option>
+              <option value="050">050</option>
+              <option value="070">070</option>
+              <option value="077">077</option>
+            </select>
+            <input
+              type="text"
+              placeholder="Telefon Nömrəsi"
+              className="input input-bordered placeholder:opacity-50 w-full bg-gray-100"
+            />
+          </div>
+          <button className="p-3 bg-yellow-400 text-black rounded-xl w-full md:w-auto">
             Təsdiq et
           </button>
         </div>
 
-        <div className="flex justify-center items-center  mt-12">
-          <h1 className="font-bold text-center md:text-3xl">Tarif paketini seç</h1>
+        <div className="flex justify-center items-center mt-8 md:mt-12">
+          <h1 className="font-bold text-center md:text-3xl">
+            Tarif paketini seç
+          </h1>
         </div>
         <div className="flex flex-col justify-center items-center gap-4 mt-5">
-          <div className="w-[300px] p-3 flex gap-2 items-center justify-around border border-gray-500">
+          <div className="w-full md:w-[400px] p-3 flex gap-2 items-center justify-around border border-gray-500">
             <input
               type="checkbox"
               className="checkbox checkbox-warning"
@@ -94,11 +104,11 @@ const Page = () => {
               checked={selectedPackage === "3azn"}
             />
             <h1>Standart - 3 gün (SuperSport daxil)</h1>
-            <button className="flex bg-yellow-400 text-black p-2 rounded-2xl ">
+            <button className="flex bg-yellow-400 text-black p-2 rounded-2xl">
               3 AZN
             </button>
           </div>
-          <div className="w-[300px] p-3 flex gap-2 items-center justify-around border border-gray-500">
+          <div className="w-full md:w-[400px] p-3 flex gap-2 items-center justify-around border border-gray-500">
             <input
               type="checkbox"
               className="checkbox checkbox-warning"
@@ -107,11 +117,11 @@ const Page = () => {
               checked={selectedPackage === "5azn"}
             />
             <h1>Standart - 3 gün (SuperSport daxil)</h1>
-            <button className="flex bg-yellow-400 text-black p-2 rounded-2xl ">
+            <button className="flex bg-yellow-400 text-black p-2 rounded-2xl">
               5 AZN
             </button>
           </div>
-          <div className="w-[300px] p-3 flex gap-2 items-center justify-around border border-gray-500">
+          <div className="w-full md:w-[400px] p-3 flex gap-2 items-center justify-around border border-gray-500">
             <input
               type="checkbox"
               className="checkbox checkbox-warning"
@@ -120,17 +130,17 @@ const Page = () => {
               checked={selectedPackage === "10azn"}
             />
             <h1>Standart - 3 gün (SuperSport daxil)</h1>
-            <button className="flex bg-yellow-400 text-black p-2 rounded-2xl ">
+            <button className="flex bg-yellow-400 text-black p-2 rounded-2xl">
               10 AZN
             </button>
           </div>
         </div>
-        <div className="flex-col text-center space-y-4 mt-12">
+        <div className="flex-col text-center space-y-4 mt-8 md:mt-12">
           <h1 className="font-bold text-3xl">Təsdiqlə və qoşul</h1>
           <p ref={qiymetRef} className="font-bold text-2xl text-red-400">
             {price} AZN
           </p>
-          <button className=" bg-yellow-400 font-bold text-black p-4 rounded-2xl shadow-lg hover:scale-105 transition-all duration-500">
+          <button className="bg-yellow-400 font-bold text-black p-4 rounded-2xl shadow-lg hover:scale-105 transition-all duration-500">
             <a href="https://short.fpay.az/gammanet"> Ödəniş et</a>
           </button>
         </div>
